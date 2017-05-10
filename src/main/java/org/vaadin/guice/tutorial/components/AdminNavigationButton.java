22ee76af606fe270941659d268dc03c8dcf555ff
berndhopp <bernd@vaadin.com>
2017-05-10 18:38:49 +0200

    init

--- src/main/java/org/vaadin/guice/tutorial/components/AdminNavigationButton.java
+++ src/main/java/org/vaadin/guice/tutorial/components/AdminNavigationButton.java
@@ -1,10 +1,13 @@
 package org.vaadin.guice.tutorial.components;
 
+import org.ilay.Authorization;
+import org.vaadin.guice.tutorial.CurrentUserRole;
 import org.vaadin.guice.tutorial.views.AdminView;
 
 public class AdminNavigationButton extends NavigationButton {
     public AdminNavigationButton() {
         super(AdminView.VIEW_NAME);
+        Authorization.restrictComponent(this).to(CurrentUserRole.ADMIN);
     }
 }
 
